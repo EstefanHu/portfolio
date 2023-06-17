04d9e5cabf2570e7b31d667a2b65b9fba4fe56c3
Estefan Hu <ehu@goodwaygroup.com>
2023-06-16 19:19:40 -0700

    feat(layout): Settled layout

--- src/app/prjkts/(projects)/page.tsx
+++ src/app/prjkts/(projects)/page.tsx
@@ -1,16 +1,53 @@
-import Link from 'next/link';
 import styles from './projects.module.scss';
+
+const PROJECTS = [
+  {
+    name: 'Quarkify',
+    url: 'https://www.quarkify.ai',
+    description: '',
+  },
+];
+
+const IDEAS = [
+  {
+    name: 'Redline',
+    url: 'https://www.quarkify.ai',
+    description: '',
+  },
+];
 
 export default function Prjkts() {
   return (
-    <div className={styles.wrapper}>
-      <section>
-        <h2>Things Im working on</h2>
+    <>
+      <section className={styles.section}>
+        <div className={styles.header}>
+          <h2>active projects</h2>
+          <p>Things Im actively working on</p>
+        </div>
+
+        {PROJECTS.map(({ name, url, description }) => {
+          return (
+            <div key={name} className='card'>
+              <h3>{name}</h3>
+            </div>
+          );
+        })}
       </section>
 
-      <section>
-        <h2>elephant graveyard</h2>
+      <section className={styles.section}>
+        <div className={styles.header}>
+          <h2>suspended ideas</h2>
+          <p>A queue of ideas</p>
+        </div>
+
+        {IDEAS.map(({ name, url, description }) => {
+          return (
+            <div key={name} className='card'>
+              <h3>{name}</h3>
+            </div>
+          );
+        })}
       </section>
-    </div>
+    </>
   );
 }
--- src/app/prjkts/layout.tsx
+++ src/app/prjkts/layout.tsx
@@ -1,21 +1,8 @@
-import Link from 'next/link';
 import styles from './layout.module.scss';
-
-const PROJECTS = [
-  {
-    name: 'Quarkify',
-    url: 'https://www.quarkify.ai',
-    description: '',
-  },
-];
 
 export default function BlgLayout({ children }: { children: React.ReactNode }) {
   return (
     <main className={styles.wrapper}>
-      <div>
-        <h1>projects</h1>
-      </div>
-
       {children}
     </main>
   );
