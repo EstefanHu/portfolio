e926e73d61bdce853ff6d3758310fc94eee0a7c2
Estefan Hu <ehu@goodwaygroup.com>
2023-06-20 22:16:07 -0700

    Corrected styles

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,4 +1,18 @@
+import Link from 'next/link';
 import styles from './landing.module.scss';
+
+const READS = [
+  {
+    title: 'Who am I',
+    url: '/blg/p/who-am-i',
+    createdAt: 'Jan 07 1996',
+  },
+  {
+    title: 'A Meta Modern Maker',
+    url: '/blg/p/a-meta-modern-maker',
+    createdAt: 'Jun 20 2023',
+  },
+];
 
 export default function Home() {
   return (
@@ -7,6 +21,18 @@ export default function Home() {
         J. <span>Estefan</span> Hu
       </h1>
       <p className={styles.headliner}>Web Developer</p>
+
+      <section>
+        <h2>Reads</h2>
+
+        {READS.map(({ title, url }) => (
+          <Link key={title} href={url}>
+            <article>
+              <h3>{title}</h3>
+            </article>
+          </Link>
+        ))}
+      </section>
     </main>
   );
 }
