a2ebd05199dbb55eb3dcd28a594cd72f763bb989
Estefan Hu <ehu@goodwaygroup.com>
2023-06-30 23:05:11 -0700

    footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
 import Link from 'next/link';
+import { FaGithubSquare, FaLinkedin } from 'react-icons/fa';
+import styles from './layout.module.scss';
 import './globals.css';
 
 import Logo from '@/components/Logo';
@@ -14,7 +16,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang='en'>
       <body>
-        <header>
+        <header className={styles.header}>
           <Link href='/'>
             <Logo />
           </Link>
@@ -33,6 +35,32 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         </header>
 
         {children}
+
+        <div className={styles.footerWrapper}>
+          <footer>
+            <div className={styles.footerTop}>
+              <div className={styles.branding}>
+                <p className={styles.name}>estefan</p>
+                <p>Software Developer</p>
+                <p>Seattle, Washington</p>
+              </div>
+            </div>
+
+            <div className={styles.legal}>
+              <span>
+                <Link target='_blank' rel='noopener noreferrer' href='https://www.linkedin.com/in/estefanhu/'>
+                  <FaLinkedin />
+                </Link>
+
+                <Link target='_blank' rel='noopener noreferrer' href='https://github.com/estefanhu/'>
+                  <FaGithubSquare />
+                </Link>
+              </span>
+
+              <p>&copy; 2023 Justin Estefan Hu - all rights reserved</p>
+            </div>
+          </footer>
+        </div>
       </body>
     </html>
   );
