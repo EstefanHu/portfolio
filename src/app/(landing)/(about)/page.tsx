7a6077d6fa419bf901350cf16bff23da281ef613
Estefan Hu <ehu@goodwaygroup.com>
2023-07-05 12:07:43 -0700

    image

--- src/app/(landing)/(about)/page.tsx
+++ src/app/(landing)/(about)/page.tsx
@@ -1,12 +1,27 @@
+import Image from 'next/image';
 import styles from './landing.module.scss';
 
 export default function Home() {
   return (
     <main className={styles.wrapper}>
-      <h1>
-        J. <span>Estefan</span> Hu
-      </h1>
-      <h2>Web Developer</h2>
+      <section className={styles.hero}>
+        <div>
+          <h1>
+            J. <span>Estefan</span> Hu
+          </h1>
+          <h2>Web Developer</h2>
+        </div>
+
+        <div className={styles.imageWrapper}>
+          <Image
+            src='/images/me.webp'
+            alt='me'
+            priority
+            objectFit='cover'
+            fill
+          />
+        </div>
+      </section>
 
       <section>
         <h3>To make, or not to make</h3>
