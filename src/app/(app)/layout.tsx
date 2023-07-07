6882796fa90efeb6cf230fbe7a9587fae89c89d2
Estefan Hu <ehu@goodwaygroup.com>
2023-07-06 22:57:30 -0700

    eod

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,10 +1,13 @@
 import styles from './app.module.scss';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className={styles.appWrapper}>
-      <header></header>
-      <main>{children}</main>
-    </div>
-  );
+    return (
+        <div className={styles.appWrapper}>
+            <header></header>
+            <main>
+                <nav></nav>
+                {children}
+            </main>
+        </div>
+    );
 }
