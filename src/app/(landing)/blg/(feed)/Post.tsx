435323353da0b8e1cbf0b589b5e2a7daaae1b624
Estefan Hu <estefanhu074@gmail.com>
2023-07-11 11:08:19 -0700

    lint

--- src/app/(landing)/blg/(feed)/Post.tsx
+++ src/app/(landing)/blg/(feed)/Post.tsx
@@ -1,7 +1,7 @@
-export default function Post({ title }) {
-    return (
-        <div>
-            <h1>{title}</h1>
-        </div>
-    )
+export default function Post({ title }: { title: string }) {
+  return (
+    <div>
+      <h1>{title}</h1>
+    </div>
+  );
 }
