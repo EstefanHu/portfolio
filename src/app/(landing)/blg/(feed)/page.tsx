b94c7745dd9218925ec5f7a72766d0adcbe70504
Estefan Hu <estefanhu074@gmail.com>
2023-07-11 09:39:21 -0700

    deps

--- src/app/(landing)/blg/(feed)/page.tsx
+++ src/app/(landing)/blg/(feed)/page.tsx
@@ -1,14 +1,19 @@
 import BlogNavigation from './BlogNavigation';
+import Post from './Post';
 import styles from './feed.module.scss';
 
 export default function Posts() {
+  const posts = [];
+
   return (
     <div className={styles.wrapper}>
       <nav>
         <BlogNavigation />
       </nav>
 
-      <div className={styles.feed}></div>
+      <div className={styles.feed}>
+        {posts.map((post) => <Post {...post} />)}
+      </div>
     </div>
   );
 }
