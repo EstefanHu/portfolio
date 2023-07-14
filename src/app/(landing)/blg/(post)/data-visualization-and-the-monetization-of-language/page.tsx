41557c390081666b47f24228f0d22a927bd9e92b
Estefan Hu <estefanhu074@gmail.com>
2023-07-13 23:09:29 -0700

    eod

---
+++ src/app/(landing)/blg/(post)/data-visualization-and-the-monetization-of-language/page.tsx
@@ -0,0 +1,20 @@
+export default function page() {
+    return (
+        <>
+            <h1>Data Visualization and the Monetization of Language</h1>
+            <h2></h2>
+
+            <p>The future of data visualization is standardization.</p>
+
+            <p>There has been amazing advancements in the world of data visualizations, from malleable libraries like D3 to more opinionated frameworks such as R shiny or even streamlit. These technologies provide robust solutions to the impossible task of communicating abstractions and reductions into digestible, bite sized iconographies.</p>
+            
+            <p>So, why standardize?</p>
+
+            <p>If there are new and amazing innovations blooming left and right that do, definitely,  improve how organizations communicate and relay data, why stifle it? Would it not be better to just let the free market of innovation and product development solve for each individual organization's problem statement, allowing for  each organization to select which is best for the particular circumstance?</p>
+
+            <p>Well, yes, and no.</p>
+
+            <p></p>
+        </>
+    )
+}
