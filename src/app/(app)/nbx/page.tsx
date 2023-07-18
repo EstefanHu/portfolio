ae32df872714bd297fe8f6529275a5b71f6fa251
Estefan Hu <estefanhu074@gmail.com>
2023-07-17 21:08:03 -0700

    lint

--- src/app/(app)/nbx/page.tsx
+++ src/app/(app)/nbx/page.tsx
@@ -1,17 +1,19 @@
 import prisma from '@/lib/prisma';
-import styles from './inbox.module.scss'
+import styles from './inbox.module.scss';
 
 export default async function Page() {
   const contacts = await prisma.contact.findMany();
 
-  return <div className={styles.main}>
-    <h1>Inbox</h1>
-    {contacts.map(({ id, name, email, message, reason, origin, status, createdAt }) => {
-      return (
-        <div key={id}>
-          <p>{name}</p>
-        </div>
-      )
-    })}
-  </div>;
+  return (
+    <div className={styles.main}>
+      <h1>Inbox</h1>
+      {contacts.map(({ id, name, email, message, reason, origin, status, createdAt }) => {
+        return (
+          <div key={id}>
+            <p>{name}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
